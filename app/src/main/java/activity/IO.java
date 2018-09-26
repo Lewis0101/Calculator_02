76450131254615e46bf3fa774a17254f36761bfc
LiuYang <1005859278@qq.com>
2018-09-26 15:13:41 +0800

    这是一次测试提交

---
+++ app/src/main/java/activity/IO.java
@@ -0,0 +1,11 @@
+package activity;
+
+/**
+ * @author : LiuYang
+ * @date : 2018/9/26
+ */
+public class IO {
+    public static void main(String[] args) {
+        System.out.println("this is a first program");
+    }
+}
---
+++ app/src/main/java/activity/Test.java
@@ -0,0 +1,15 @@
+package activity;
+
+/**
+ * @author : LiuYang
+ * @date : 2018/9/26
+ */
+public class Test extends MainActivity {
+    public static void main(String[] args) {
+        int a = 10;
+        for(int i = 0; i< 10 ;i++)
+        {
+            System.out.println(a+i);
+        }
+    }
+}
